5f3f2f14c211cdca991cc7c67bf958225c025010
Paweł Niedźwiecki <hello@uxu.pl>
2022-08-08 18:55:11 +0200

    [ feat ]: create settings for GlobalStyle & types for settings

---
+++ src/assets/style/settings.ts
@@ -0,0 +1,30 @@
+import { MainSettingsTemplate } from "utils/types/utils.types.mainSettingsTemplate";
+
+const settings: MainSettingsTemplate = {
+  // settings color
+  white: "#FFFFFF",
+  succes: "#318841",
+  desactive: "#AFAFAF",
+  mainBackGround:
+    "linear-gradient(167deg, rgba(44,90,114,1) 0%, rgba(23,30,40,1) 38%, rgba(30,31,39,1) 45%, rgba(51,33,37,1) 87%, rgba(52,33,37,1) 100%)",
+
+  // settings break
+  break: { small: "0.75rem", main: "1.5rem", big: "3rem" },
+
+  // settings breakpoint
+  breakPoint: [
+    { type: "xs", break: "0px" },
+    { type: "sm", break: "540px" },
+    { type: "md", break: "768px" },
+    { type: "lg", break: "960px" },
+    { type: "xl", break: "1140px" },
+    { type: "xxl", break: "1320px" },
+  ],
+
+  gridCol: 12,
+
+  // settings max width
+  max_width: "128rem",
+};
+
+export default settings;
---
+++ src/utils/types/utils.types.mainSettingsTemplate.ts
@@ -0,0 +1,11 @@
+export type MainSettingsTemplate = {
+  white: string;
+  succes: string;
+  desactive: string;
+  mainBackGround: string;
+
+  break: { small: string; main: string; big: string };
+  breakPoint: { type: string; break: string }[];
+  gridCol: number;
+  max_width: string;
+};
