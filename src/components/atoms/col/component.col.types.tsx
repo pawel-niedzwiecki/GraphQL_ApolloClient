54ee293b264efd64ac9969565054c69a8f189bbe
Paweł Niedźwiecki <hello@uxu.pl>
2022-08-08 20:37:06 +0200

    [ feat ] add col for gridSystem

---
+++ src/components/atoms/col/component.col.types.tsx
@@ -0,0 +1,21 @@
+export interface ColPropsTypes {
+  xs: number;
+  sm: number;
+  md: number;
+  lg: number;
+  xl: number;
+  xxl: number;
+}
+
+export interface ColComponentPropsTypes {
+  xs?: number;
+  sm?: number;
+  md?: number;
+  lg?: number;
+  xl?: number;
+  xxl?: number;
+  style?: any;
+  className?: string;
+  ref?: JSX.Element | JSX.Element[] | any;
+  children?: JSX.Element | JSX.Element[] | any;
+}
