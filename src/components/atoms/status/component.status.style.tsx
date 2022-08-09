33d6699bed582671c86e5bb6952ea400ab9a3b22
Paweł Niedźwiecki <hello@uxu.pl>
2022-08-09 16:08:31 +0200

    [ fix ]: fix styled for component status

--- src/components/atoms/status/component.status.style.tsx
+++ src/components/atoms/status/component.status.style.tsx
@@ -4,8 +4,11 @@ import { ComponentListTypes, ComponentStatusEnumTypes } from "./component.status
 
 export const Status = styled.ul<ComponentListTypes>`
   display: block;
+  font-weight: bold;
   width: max-content;
   position: relative;
+  font-size: 1rem;
+  border-radius: 1.5rem;
   color: ${({ theme }: { theme: MainSettingsTemplate }) => theme.colorWhite};
   padding: ${({ theme }: { theme: MainSettingsTemplate }) => `${theme.break.small} ${theme.break.main}`};
   background-color: ${({ theme, type }: { theme: MainSettingsTemplate; type: ComponentStatusEnumTypes }) =>
