f4ae42f31d2f5c520d620d46a2c9cdb11ec6e215
Paweł Niedźwiecki <hello@uxu.pl>
2022-08-09 15:25:54 +0200

    [ feat ] add logic to component with list

--- src/components/atoms/list/component.list.style.tsx
+++ src/components/atoms/list/component.list.style.tsx
@@ -1,6 +1,23 @@
 import styled from "styled-components";
-import { ComponentListTypes } from "./component.list.types";
+import { MainSettingsTemplate } from "utils/types/utils.types.mainSettingsTemplate";
+import { ComponentListTypes, ConponentListEnumTypes } from "./component.list.types";
 
-export const List = styled.ul<ComponentListTypes>``;
+export const List = styled.ul<ComponentListTypes>`
+  display: flex;
+  width: max-content;
+  position: relative;
+  flex-wrap: ${({ type }: ComponentListTypes) => (type === ConponentListEnumTypes.level ? "nowrap" : "wrap")};
+`;
 
-export const Item = styled.li``;
+export const Item = styled.li`
+  width: 100%;
+  position: relative;
+  white-space: nowrap;
+  display: inline-block;
+  padding: ${({ theme }: { theme: MainSettingsTemplate }) => theme.break.small};
+
+  span {
+    padding-right: 0.3rem;
+    color: ${({ theme }: { theme: MainSettingsTemplate }) => theme.colorDesactive};
+  }
+`;
