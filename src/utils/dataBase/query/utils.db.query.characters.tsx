cb523ae3691564c87576ba6717a4b08f4255c7c6
Paweł Niedźwiecki <hello@uxu.pl>
2022-08-10 21:52:47 +0200

    [ fix ] update query to db for characters

--- src/utils/dataBase/query/utils.db.query.characters.tsx
+++ src/utils/dataBase/query/utils.db.query.characters.tsx
@@ -3,6 +3,10 @@ import { gql } from "@apollo/client";
 export const GET_CHARACTERS = gql`
   query Search($page: Int!, $name: String!) {
     characters(page: $page, filter: { name: $name }) {
+      info {
+        next
+        pages
+      }
       results {
         id
         name
