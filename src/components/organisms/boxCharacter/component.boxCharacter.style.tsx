bc48ce534a86e4bb9b0e1113bbf4b74e2c5eddb8
Paweł Niedźwiecki <hello@uxu.pl>
2022-08-14 12:56:52 +0200

    [ fix ] fix style to box character

--- src/components/organisms/boxCharacter/component.boxCharacter.style.tsx
+++ src/components/organisms/boxCharacter/component.boxCharacter.style.tsx
@@ -2,13 +2,14 @@ import styled from "styled-components";
 import { MainSettingsTemplate } from "utils/types/utils.types.mainSettingsTemplate";
 import {} from "./component.boxCharacter.types";
 
-export const Box = styled.ul`
+export const Box = styled.div`
   width: 100%;
+  height: calc(100% - 3rem);
   display: block;
   overflow: hidden;
   position: relative;
   border-radius: 0.3rem;
-  margin: ${({ theme }: { theme: MainSettingsTemplate }) => theme.break.small} 0;
+  margin: ${({ theme }: { theme: MainSettingsTemplate }) => theme.break.main} 0;
   border: 0.3rem solid ${({ theme }: { theme: MainSettingsTemplate }) => theme.colorWhite};
 
   ul {
