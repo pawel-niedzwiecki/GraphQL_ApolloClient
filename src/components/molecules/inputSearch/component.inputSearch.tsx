1c3dc4dde3416bc9beb26d0799bd220b7bbf9b00
Paweł Niedźwiecki <hello@uxu.pl>
2022-08-15 03:24:25 +0200

    [ fix ] update sugest in search

--- src/components/molecules/inputSearch/component.inputSearch.tsx
+++ src/components/molecules/inputSearch/component.inputSearch.tsx
@@ -5,6 +5,7 @@ import List from "components/atoms/list/component.list";
 import { ComponentInputSearchPropsTypes, SugestSearchType } from "./component.inputSearch.types";
 import { Wrapper, Input, Sugests } from "./component.inputSearch.style";
 import { ConponentListEnumTypes } from "components/atoms/list/component.list.types";
+import { InputType } from "zlib";
 
 const ComponentInputSearch = ({ sugests = [], valueDefault, callBack }: ComponentInputSearchPropsTypes): JSX.Element => {
   const [value, setValue] = useState(valueDefault);
@@ -30,6 +31,7 @@ const ComponentInputSearch = ({ sugests = [], valueDefault, callBack }: Componen
           }, 150)
         }
         onChange={(e) => setValue(e.target.value)}
+        onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>): false | void => e.keyCode === 13 && setActive(false)}
         type="search"
         id="inputSearch"
         name="query"
